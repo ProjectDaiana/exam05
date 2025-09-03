53769f7dea4b314a867068790c67b37f8ab383c0
Daiana <dairotchen@gmail.com>
2025-09-03 12:39:43 +0200

    vect2: cannonical, maths op w vect and scalar, ostream op, index operator

--- Level1/vect2/vect2.hpp
+++ Level1/vect2/vect2.hpp
@@ -1,4 +1,5 @@
 #pragma	once
+#include <iostream>
 
 class vect2 {
 	public:
@@ -9,15 +10,16 @@ class vect2 {
 		vect2() : x(0), y(0) {}
 		vect2(int xParam, int yParam) : x(xParam), y(yParam) {}
 		vect2(const vect2 &other) : x(other.x), y(other.y) {}
-		vect2& operator=(const vect2 &other){
-			if(*this != &other){
-				this->x = other.x;
-				this->y = other.y;
+		vect2& operator=(const vect2& other) {
+			if(*this != other) {
+				x = other.x;
+				y = other.y;
 			}
 			return *this;
 		}
 		~vect2() {}
 
+		// operations, parameter scalar
 		vect2 operator+(const int scalar){
 			return vect2(x + scalar, y + scalar);
 		}
@@ -30,58 +32,52 @@ class vect2 {
 			return vect2(x - scalar, y - scalar);
 		}
 
-		vect2 operator+(const int scalar) {
-			this->x = this->x + scalar;
-			this->y = this->y + scalar;
+
+		// operations, param vector
+		vect2& operator+(const vect2& other) {
+			this->x = x + other.x;
+			this->y = y + other.y;
+			return *this;
 		}
 
-		vect2& operator-(const int scalar) {
-			this->x = this->x - scalar;
-			this->y = this->y - scalar;
+		vect2& operator-(const vect2& other) {
+			x = this->x - other.x;
+			y = this->y - other.y;
+			return *this;
 		}
 
-		vect2& operator*(const int scalar) {
-			this->x = this->x * scalar;
-			this->y-= this->y * scalar;
-		}
-
-
-		vect2& operator+(const other& vect) {
-			return vect2((this->x + other.x), (this->y + other.y));
-		}
-
-		vect2& operator-(const other& vect) {
-			return vect2((this->x - other.x), (this->y - other.y));
-		}
-
-		vect2& operator*(const other& vect) {
-			return vect2((this->x * other.x), (this->y * other.y));
+		vect2& operator*(const vect2& other) {
+			x = this->x * other.x;
+			y = this->y * other.y;
+			return *this;
 		}
 
 
-		vect2& operator+(const other& vect) {
-			this->x = this->x + other.x;
-			this->y = this->y + other.y;
+		//increment operators ++v, v--, ++v, --v
+
+		vect& operator++() {
+			x += x;
+			y +=y;
+			return *this;
+		} 
+		
+		vect& operator++(int) {
+			vect2 tmp(*this);
+			x++;
+			y++;
+			return tmp;
 		}
 
-		vect2& operator-(const other& vect) {
-			this->x = this->x - other.x;
-			this->y = this->y - other.y;
-		}
 
-		vect2& operator*(const other& vect) {
-			this->x = this->x * other.x;
-			this->y-= this->y * other.y;
-		}
+		// operator []
+		int operator[](int it) {
+			return (it == 0 ? x : y);
+		};
 
-		//operator
-		std::ostream& operator<<(std::ostream& os, const vect2& vect){
-			os << vect.x << ", " << vect.y << std::endl:
+		// operator <<
+		std::ostream& operator<<(std::ostream& os, const vect2& vect) {
+			os << "{" << vect.x << ", " << vect.y << "}" << std::endl;
 			return os;
 		}
-	
-		vect2 operator[]{};
-
-		std::vector
 
 };
